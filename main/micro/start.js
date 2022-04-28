dae2272d51ed1d189b885295905a32477463f3cd
lml <soalin228@163.com>
2022-04-28 23:06:57 +0800

    feat: 微前端预加载

--- main/micro/start.js
+++ main/micro/start.js
@@ -2,6 +2,7 @@ import { setList, getList } from './const/subApps'
 import { setMainLifecycle } from './const/mainLifeCycle'
 import { rewriteRouter } from './router/rewriteRouter'
 import { currentApp } from './utils'
+import { prefetch } from './loader/prefetch'
 import { Custom } from './customevent'
 
 const custom = new Custom()
@@ -26,10 +27,22 @@ export const start = () => {
   }
   // 有子应用的内容， 查找到符合当前路由的子应用
   const app = currentApp()
-  if (app) {
-    const { pathname, hash } = window.location
+
+  const { pathname, hash } = window.location
+
+  if (!hash) {
+    // 当前没有在使用的子应用
+    // 1. 抛出一个错误，请访问正确的连接
+    // 2. 访问一个默认的路由，通常为首页或登录页面
+    window.history.pushState(null, null, '/vue3#/index')
+  }
+
+  if (app && hash) {
     const url = pathname + hash
     window.__CURRENT_SUB_APP__ = app.activeRule
     window.history.pushState('', '', url)
   }
+
+  // 预加载 - 加载接下来的所有子应用，但是不显示
+  prefetch()
 }
